59a840ce4a0a75950ea0a48ee8c91022c5564cd3
aaryanj-0501 <aaryan.j@glideclouds.com>
2026-01-08 12:12:16 +0530

    Refactored vector api code and added test cases

--- api_vectors_ollama_07_01_26/routes/vector_routes.py
+++ api_vectors_ollama_07_01_26/routes/vector_routes.py
@@ -1,76 +1,32 @@
 from fastapi import APIRouter
-import ollama
 import os
-import uuid
+from dotenv import load_dotenv
 from models.vector import VectorDocs,VectorQuery
-from config.db import client
+from services.vector_services import add_vector,search_vector,result
 
+load_dotenv()
 router=APIRouter()
-embed_model=os.getenv("EMBED_MODEL")
 
 #HOME
 @router.get("/")
 def welcome():
     return {"message":"Welcome to Vector Task Day2!"}
 
-def get_embedding(text:str):
-    response=ollama.embeddings(
-        model='nomic-embed-text',
-        prompt=text,
-    )
-
-    return response["embedding"]
-
-def build_point(text:str):
-    embedding=get_embedding(text)
-
-    return ({
-        "id":str(uuid.uuid4()),
-        "vector": embedding,
-        "payload":{
-            "text": text
-        }
-    })
-
 #Query
 @router.post("/vector-doc")
 async def store_docs(document:VectorDocs):
-    doc=document.docs
-    
-    client.upsert(
-        collection_name='vector_crud',
-        points=[build_point(doc)]
-    )
+    await add_vector(document.docs,"document")
 
     return {"status":"Document stored successfully"}
 
 @router.post("/vector-query")
 async def search_query(q:VectorQuery, limit:int=3):
     query=q.query
-    point=build_point(query)
-    client.upsert(
-        collection_name='queries',
-        points=[point]
-    )
+    await add_vector(query,"query")
 
-    search_result=client.query_points(
-        collection_name='vector_crud',
-        query=get_embedding(query),
-        limit=limit,
-        with_payload=True
-    )
+    search_result=await search_vector(query,limit)
 
-    return {
-        "query":query,
-        "results":[
-            {
-                "id":point.id,
-                "score":point.score,
-                "payload":point.payload
-            }
-            for point in search_result.points
-        ]
-    }
+    return await result(search_result,query)
 
 
 
