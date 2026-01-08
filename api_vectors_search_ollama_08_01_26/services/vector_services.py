bf61da9f5f83f3aa23d240fe04b222846911acaa
aaryanj-0501 <aaryan.j@glideclouds.com>
2026-01-08 19:08:25 +0530

    Vector dump added

--- api_vectors_search_ollama_08_01_26/services/vector_services.py
+++ api_vectors_search_ollama_08_01_26/services/vector_services.py
@@ -3,6 +3,8 @@ import uuid
 from config.db import client
 import os
 from dotenv import load_dotenv
+from typing import Optional
+import json
 
 load_dotenv()
 embed_model=os.getenv("EMBED_MODEL")
@@ -14,7 +16,6 @@ def get_embedding(text:str):
         model=embed_model,
         prompt=text,
     )
-
     return response["embedding"]
 
 def build_point(text:str):
@@ -56,3 +57,26 @@ async def result(search_result,query:str):
             for point in search_result.points
         ]
     }
+
+def export_collection_to_json(collection_name:str,output_file:str="exported_collection.json"):
+    all_points=[]
+    offset:Optional[str]=None
+    
+    while True:
+        points,offset=client.scroll(
+            collection_name=collection_name,
+            limit=100,
+            offset=offset,
+            with_payload=True
+        )
+
+        for point in points:
+            all_points.append({
+                "id":point.id,
+                "payload":point.payload
+            })
+        if offset is None:
+            break
+
+    with open(output_file,"w",encoding="utf-8") as f:
+        json.dump(all_points,f,ensure_ascii=False,indent=2)
